f2772d584205252e83ec549bf672f43ef4c07536
nengmaya538 <nengmaya538@gmail.com>
2025-09-24 00:54:55 +0700

    Update hub.js

--- functions/hub.js
+++ functions/hub.js
@@ -1,5 +1,5 @@
 export async function onRequest(context) {
-  const zoneId = "juHQEuclG1tLGb6fJKLkcGH0xQp_2vWRlOLUvAME"; // Ganti dengan Zone ID domain kamu
+  const zoneId = "3b33f4c897d3409d3d0cd2eedd312e9f"; // Ganti dengan Zone ID domain kamu
   const apiToken = context.env.CF_API_TOKEN; // Tambahkan secret di Pages
 
   const url = `https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records?per_page=100`;
